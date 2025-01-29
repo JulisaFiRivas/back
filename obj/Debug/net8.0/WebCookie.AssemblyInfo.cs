f19f64daa9830a4fb70b68ec0d06235761a7fd81
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-01-29 11:20:57 -0300

    cambios en base de datos

--- obj/Debug/net8.0/WebCookie.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCookie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCookie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1e567cba7a5d91711036f002fe139572a71587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99653238dfabc7101dbfea4ab1851015d590ae1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCookie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCookie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
