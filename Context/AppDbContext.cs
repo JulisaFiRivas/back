11232c52e3ce25bc294eda04eff45b332951217c
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-01-29 16:05:31 -0300

    probando

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace WebCookie.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options){ }
-
+        //comentrio para saber si el git esta funcioinado
         public DbSet<Cookie> Cookies { get; set; }
 
     }
